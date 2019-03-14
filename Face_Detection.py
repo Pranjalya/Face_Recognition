c39fbf49fde0465c5610fb2bad464c6542c4641c
Pranjalya <36627085+Pranjalya@users.noreply.github.com>
2019-03-14 18:10:19 +0530

    Updated face detection program.

--- Face_Detection.py
+++ Face_Detection.py
@@ -3,26 +3,29 @@ import cv2
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture(0)
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+capture = cv2.VideoCapture(0)
+capture.set(3,640) # set Width
+capture.set(4,480) # set Height
+
+print("Press ESC to quit.")
 
 while True:
-    ret, img = cap.read()
+    ret, img = capture.read()
     img = cv2.flip(img, +1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5, minSize=(20, 20))
+    faces_color = faceCascade.detectMultiScale(img, scaleFactor=1.2, minNeighbors=5, minSize=(25, 25))
+    
+    for (x,y,w,h) in faces_color:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(100,89,123),2)
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(gray,(x,y),(x+w,y+h),(120,120,120),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]  
-
-    cv2.imshow('video', gray)
-
-    k = cv2.waitKey(30)
-    if k == 27: # press 'ESC' to quit
+   # cv2.putText(img, "Kvothe", (x+5,y-5), cv2.FONT_HERSHEY_COMPLEX, 1, (120,123,156), 2, cv2.LINE_AA)
+    cv2.imshow('Testing_', img)
+    
+    keypress = cv2.waitKey(30)
+    if keypress == 27: # press 'ESC' to quit
         break
 
-cap.release()
+capture.release()
 cv2.destroyAllWindows()
