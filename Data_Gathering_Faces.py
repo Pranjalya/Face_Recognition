f94c2e613d68da7550a5944da10c191b303f5559
Pranjalya <36627085+Pranjalya@users.noreply.github.com>
2019-03-14 17:56:13 +0530

    Add files via upload

--- Data_Gathering_Faces.py
+++ Data_Gathering_Faces.py
@@ -8,8 +8,13 @@ cam.set(4, 480) # set video height
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # For each person, enter one numeric face id
-face_id = input('\n Enter User Id and press Enter, please : ')
+face_name = raw_input('\n Enter User Name and press Enter, please : ')
 print("\n Look at the camera and smile!")
+
+try:
+    os.mkdir("tf_files/Faces_Datasets/"+face_name)
+except:
+    print("Continue. Folder exists.")
 
 # Initialize individual sampling face count
 count = 0
@@ -18,22 +23,34 @@ while(True):
     ret, img = cam.read()
     img = cv2.flip(img, +1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+    faces = face_detector.detectMultiScale(img , 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 1)     
         count += 1
+        print("Testing entry ", count)
+       
+        cv2.imshow('image', img)
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("Faces_Datasets/" + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+            # Press 'ESC' for exiting video       
+        
+        if count >= 505: # Take 3000 face sample and stop video
+            break
 
-        cv2.imshow('image', gray)
+         # Save the captured image into the datasets folder
+        cv2.imwrite("tf_files/Faces_Datasets/" + face_name + '/' + str(count) + ".jpg", img[y:y+h,x:x+w])
 
-    k = cv2.waitKey(100) # Press 'ESC' for exiting video
-    if k == 27:
+        # Image is 126 x 126
+
+        cv2.imshow('image', img)
+
+        k = cv2.waitKey(1) & 0xff
+        if k == 27:
+            break
+        
+    if count >= 505: # Take 3000 face sample and stop video
         break
-    elif count >= 30: # Take 50 face sample and stop video
-         break
 
+    
 cam.release()
 cv2.destroyAllWindows()
